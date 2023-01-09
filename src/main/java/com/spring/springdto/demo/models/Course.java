1129875dab8f47504bc7bd9475c47a124505765e
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-01-09 22:56:22 +0200

    add config of mysql

--- src/main/java/com/spring/springdto/demo/models/Course.java
+++ src/main/java/com/spring/springdto/demo/models/Course.java
@@ -1,7 +1,12 @@
 package com.spring.springdto.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
 
+    @Id
     private Long id;
 
     private String name;
--- src/main/java/com/spring/springdto/demo/models/Student.java
+++ src/main/java/com/spring/springdto/demo/models/Student.java
@@ -1,10 +1,12 @@
 package com.spring.springdto.demo.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Student {
 
+    @Id
     private Long id;
 
     private String name;
