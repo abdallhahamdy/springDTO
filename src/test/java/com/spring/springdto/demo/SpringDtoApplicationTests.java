99582ac9e0bd31cc434c3d73a00ca776abe7a327
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-01-09 21:29:12 +0200

    create project dto

---
+++ src/test/java/com/spring/springdto/demo/SpringDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springdto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
