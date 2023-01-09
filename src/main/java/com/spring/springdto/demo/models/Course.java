f24ba33932fcc74c932b2b6018719b33d8460ae1
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-01-09 22:33:32 +0200

    create Layer and models (studnt - course)

---
+++ src/main/java/com/spring/springdto/demo/models/Course.java
@@ -0,0 +1,12 @@
+package com.spring.springdto.demo.models;
+
+public class Course {
+
+    private Long id;
+
+    private String name;
+
+    private String cost;
+
+    private String time;
+}
---
+++ src/main/java/com/spring/springdto/demo/models/Student.java
@@ -0,0 +1,13 @@
+package com.spring.springdto.demo.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student {
+
+    private Long id;
+
+    private String name;
+
+    private String age;
+}
